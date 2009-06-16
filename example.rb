dd53e74637c8983559bb5e13e49f26154557603f
Osamu Matsumoto <osamu.matsumoto@gmail.com>
2009-06-16 22:14:18 +0900

    remove

--- example.rb
+++ example.rb
@@ -4,11 +4,11 @@ require 'trapfilter'
 
 tf = TrapFilter.new(:policy =>"drop") # when all rules true, ignore trap
 tf.import(STDIN)
-tf.add_rule('HOST' => 'sgi-vpn-srv.localdomain', 
+tf.add_rule('HOST' => 'test-srv.localdomain', 
             'IP' => '10.0.1.1',  
             'NETSCREEN-TRAP-MIB::netscreenTrapDesc.0' => '[Root]system-critical-00027')
 
-tf.add_rule('HOST' => 'sgi-vpn-srv', 
+tf.add_rule('HOST' => 'test-vpn-srv', 
             'IP' => '10.0.1.1',  
             'NETSCREEN-TRAP-MIB::netscreenTrapDesc.0' => '[Root]system-critical-00027')
 
